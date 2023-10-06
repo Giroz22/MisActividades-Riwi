eef9a20a53149623ac6e5998fdc5b777f554f585
Alejandro Giraldo Serna <147103562+Giroz22@users.noreply.github.com>
2023-10-06 14:25:19 -0500

    Correccion multiplo-3y5

--- desarrollo/Oct 6/condicionales/problemas/multiplo-3y5.py
+++ desarrollo/Oct 6/condicionales/problemas/multiplo-3y5.py
@@ -1,25 +1,33 @@
+# - Problema, en base a los numeros igresados se imprime si es multiplo de 3 y 5 o no
 def encotrarMultiplo():
-    try:          
-        num = int(input("Ingrese un numero:\n"))                
+    try:
+        # Declaramos las listas                                
         multiplosde3y5 = []
-        noMultiplosde3y5 = []
-        msj = ""
+        noMultiplosde3y5 = []        
         
+        # pedimos los datos 3 veces
         for i in range(3):
+            num = int(input("Ingrese un numero:\n"))  
             esMultiplo3y5 = validarMultiplo3y5(num)
             
+            # guardamos el numero en una lista u otra
             if(esMultiplo3y5):
                 multiplosde3y5.append(num)
-            else:
-                noMultiplosde3y5.append(num)
+            else:                
+                noMultiplosde3y5.append(num)                
         
-        # for num in multiplosde3y5:
-        #     print(num)
-            
-        # for num in noMultiplosde3y5:
-        #     print(num)
+        # - Imprimimos los numeros que son multiplos de 3 y 5        
+        msj = "Multiplos de 3 y 5: " 
+        for num in multiplosde3y5:
+            msj += "\n" + str(num)
+               
+        # - Creamos un mensaje con los numeros que no son multiplos de 3 y 5
+        msj += "\nNo son multiplos de 3 y 5: "
+        for num in noMultiplosde3y5:
+            msj += "\n" + str(num)
         
-        #print(msj) 
+        # - Imprimimos el mensaje
+        print(msj) 
         
     except ValueError:
         print("Ingrese un valor valido")
@@ -33,6 +41,4 @@ def validarMultiplo3y5(num):
     else:
         return False            
     
-                
-
 encotrarMultiplo()
