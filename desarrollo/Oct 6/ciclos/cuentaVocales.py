f104f8fbb6bc4d9d9fd5816fbcbd990c79ee720e
Alejandro Giraldo Serna <147103562+Giroz22@users.noreply.github.com>
2023-10-09 09:00:54 -0500

    Update cuentaVocales.py

--- desarrollo/Oct 6/ciclos/cuentaVocales.py
+++ desarrollo/Oct 6/ciclos/cuentaVocales.py
@@ -1,3 +1,4 @@
+# - Cuenta el número de vocales que hay en una palabra 
 def cuentaVocales():   
     texto = input("Ingrese una cadena de texto: \n")     
     numVocales = sum(map(texto.lower().count, "aeiouáéíóú"))
